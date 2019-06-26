b7a5749fa95cf67e9c4a758e9e152b546e47eed1
s.svetlakov <s.svetlakov@mhp.com.ua>
2019-06-26 16:46:51 +0300

    oop v1

---
+++ oop/sample.py
@@ -0,0 +1,22 @@
+#Each employee has: first name, second name, salary, experience (years)
+class Department(object):
+    def __init__(self,label,managers[]):
+        self.label = label
+        self.managers[] = managers[]
+
+class Employee(object):
+    def __init__(self,name,second_name,salary,expirience_years,main_manager):
+        self.name = name
+        self.second_name = second_name
+        self.salary = salary
+        self.expirience_years = expirience_years
+        self.main_manager = dep_manager
+
+class Designer(Employee):
+    pass
+
+class Manager(Employee):
+    pass
+
+class Developer(Employee):
+    pass
