f456cd66b7443d732b3f5b7d118e6b629efbaf9f
c4n2012 <c4n2012@gmail.com>
2019-07-11 19:31:36 +0300

    yaml converter v1

--- query_app/flask_query_api.py
+++ query_app/flask_query_api.py
@@ -33,7 +33,7 @@ def returnAll():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5005)
+    app.run(host='127.0.0.1', port=5003)
 
 
 
--- query_app/post_request_to_index.py
+++ query_app/post_request_to_index.py
@@ -1,13 +1,17 @@
 import requests, json
 import yaml
+
 api_url = "https://api.worldtradingdata.com/api/v1/mutualfund?symbol=AAAAX,AAADX,AAAGX&api_token=demo"
-res = requests.get(api_url)
-# decoded_response = res.decode("UTF-8")
-data = res.json()
 
-print(data)
-print(res.status_code)
+def get_data_from_remote_api(api_url):
+    res = requests.get(api_url)
+    # decoded_response = res.decode("UTF-8")
+    data = res.json()
+    yml_data = yaml.dump(data)
+    print(yml_data)
+    print(res.status_code)
 
+get_data_from_remote_api(api_url)
 
 # get_response = requests.post('https://api.worldtradingdata.com/api/v1/stock?symbol=AMZN&api_token=0tkx6y1tk3MUvuaNBrye2iIhBACthLoa0gcLHTUSvON2qU7nHKHgmaHMucGn')
 # requests.post('https://httpbin.org/post', data={'key':'value'})
